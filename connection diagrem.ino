be7f5d55188c45742e085c812007c4a4ea728d08
sanal m s <131573751+sanal-m-s@users.noreply.github.com>
2026-03-25 11:28:23 +0530

    Update and rename new.ino to connection diagrem.ino

---
+++ connection diagrem.ino
@@ -0,0 +1,18 @@
+/*
+| S. No | Component              | Pin on Component | Connected to ESP32 Pin |
+| ----: | ---------------------- | ---------------- | ---------------------- |
+|     1 | RFID Reader (RC522)    | SDA (SS)         | GPIO 5                 |
+|     2 | RFID Reader (RC522)    | SCK              | GPIO 18                |
+|     3 | RFID Reader (RC522)    | MOSI             | GPIO 23                |
+|     4 | RFID Reader (RC522)    | MISO             | GPIO 19                |
+|     5 | RFID Reader (RC522)    | RST              | GPIO 27                |
+|     6 | RFID Reader (RC522)    | VCC              | 3.3V                   |
+|     7 | RFID Reader (RC522)    | GND              | GND                    |
+|     8 | OLED Display (SSD1306) | SDA              | GPIO 21                |
+|     9 | OLED Display (SSD1306) | SCL              | GPIO 22                |
+|    10 | OLED Display (SSD1306) | VCC              | 3.3V                   |
+|    11 | OLED Display (SSD1306) | GND              | GND                    |
+|    12 | Buzzer                 | Positive (+)     | GPIO 26                |
+|    13 | Buzzer                 | Negative (–)     | GND                    |
+|    14 | ESP32                  | USB Port         | Power Supply           |
+*/
